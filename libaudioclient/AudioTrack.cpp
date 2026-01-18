4dda14fa73fd3b092d862cb26c9d8cd425bef37f
Sergei Vasilev <svasiliev22@yandex.ru>
2026-01-18 11:28:57 +0300

    Baikalify 1
    Change-Id: I01a53c20a85906d91ec87f87b50e5656fff53d36

--- libaudioclient/AudioTrack.cpp
+++ libaudioclient/AudioTrack.cpp
@@ -120,4 +120,21 @@ _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_
 }
 #endif
 
+extern "C" {
+status_t _ZN7android11AudioSystem24setDeviceConnectionStateE24audio_policy_dev_state_tRKNS_5media5audio6common9AudioPortE14audio_format_tb(
+    audio_policy_dev_state_t state,
+    const android::media::audio::common::AudioPort& port,
+    audio_format_t encodedFormat, bool deviceSwitch);
+
+status_t _ZN7android11AudioSystem24setDeviceConnectionStateE24audio_policy_dev_state_tRKNS_5media5audio6common9AudioPortE14audio_format_t(
+    audio_policy_dev_state_t state,
+    const android::media::audio::common::AudioPort& port,
+    audio_format_t encodedFormat, bool deviceSwitch
+) {
+    return _ZN7android11AudioSystem24setDeviceConnectionStateE24audio_policy_dev_state_tRKNS_5media5audio6common9AudioPortE14audio_format_tb(
+        state, port, encodedFormat, deviceSwitch);
+}
+}
+
+
 }  // namespace android
